14c714037aafbe2a697a56f196522d7fedadb4d1
Doidae <andyarredondo1224@gmail.com>
2024-06-27 16:55:43 -0400

    Setup index.js backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const cors = require('cors')
+
+dotenv.confg()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+//Connect Mongoose
+
+//Routes

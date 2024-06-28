3d303d3be5d35d8c52c17b37509386dd62170dfd
Doidae <andyarredondo1224@gmail.com>
2024-06-27 21:09:26 -0400

    Added HomePage, Navbar and styles

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style/HomePage.css'
+import { Link } from 'react-router-dom'
+
+const HomePage = () => {
+    return (
+        <div>
+            <div>
+                <h1>Welcome To The Home Page</h1>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage

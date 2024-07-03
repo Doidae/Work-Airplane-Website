5863abb4fba0b31ab99ea9dcb7afdd51a307a068
Doidae <andyarredondo1224@gmail.com>
2024-07-03 18:07:33 -0400

    Made homepage and started contact us page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -5,17 +5,18 @@ import { Link } from 'react-router-dom'
 const HomePage = () => {
     return (
         <div className="homepage-container">
+            
             <div className="intro-container">
-                <h1>Welcome to Larry's Airplane Service!</h1>
-                <p>Schedule a flight with us today!</p>
-                <Link to='/contactus' className="navbar-button2">Contact Us</Link>
+                <h1 className="main-title">Welcome to Larry's Airplane Service!</h1>
+                <p className="subtitle">Schedule a flight with us today!</p>
+                <Link to='/contactus' className="cta-button">Contact Us</Link>
             </div>
             <div className="image-container">
-                <img src="https://via.placeholder.com/400" alt="Game Achievements" />
+                <img src="https://via.placeholder.com/400" alt="Airplane" className="main-image" />
             </div>
-            
         </div>
     );
+    
 };
 
 export default HomePage

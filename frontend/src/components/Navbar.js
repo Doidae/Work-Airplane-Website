039a5e2996206aa25ea854cf8a33034d68915c0d
Doidae <andyarredondo1224@gmail.com>
2024-07-04 18:58:37 -0400

    Adjusted navbar styling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,17 +1,27 @@
-import React from "react"
-import './style/Navbar.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './style/Navbar.css';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-    return(
-        <nav className = 'navbar'>
+    return (
+        <nav className='navbar'>
+            <div className='navbar-left'>
+                <img src='/path/to/logo.png' alt='Logo' className='navbar-logo' />
+                <span className='navbar-title'>My Website</span>
+            </div>
             <div className='navbar-right'>
-                <Link to='/'>Home</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/contactus'>Contact Us</Link>
+                <NavLink exact to='/' activeClassName='active' className='navbar-link'>
+                    Home
+                </NavLink>
+                <NavLink to='/about' activeClassName='active' className='navbar-link'>
+                    About
+                </NavLink>
+                <NavLink to='/contactus' activeClassName='active' className='navbar-link'>
+                    Contact Us
+                </NavLink>
             </div>
         </nav>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;

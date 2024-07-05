8003c1ca9efaa2c29d2bcab87fe8669c24fd446c
Doidae <andyarredondo1224@gmail.com>
2024-07-05 15:55:22 -0400

    Adjusted Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,8 +6,8 @@ const Navbar = () => {
     return (
         <nav className='navbar'>
             <div className='navbar-left'>
-                <img src='/path/to/logo.png' alt='Logo' className='navbar-logo' />
-                <span className='navbar-title'>My Website</span>
+                {/* <img src='./frontend/airplane.jpg' alt='Logo' className='navbar-logo'/> */}
+                <span className='navbar-title'>LAS</span>
             </div>
             <div className='navbar-right'>
                 <NavLink exact to='/' activeClassName='active' className='navbar-link'>

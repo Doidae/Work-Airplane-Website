9a8dd1137688c91bbd13161641b96ed732a05457
Doidae <andyarredondo1224@gmail.com>
2024-07-05 19:44:10 -0400

    Added content to about page and started styling

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -4,8 +4,20 @@ import './style/About.css'
 
 const About = () => {
     return(
-        <div>
-            <h1>This is the about page</h1>
+        <div className='about-container'>
+            <div>
+                <div>
+                    <h1>About LAS</h1>
+                </div>
+                <div>
+                    <img src='https://via.placeholder.com/400' alt='Placeholder'/>
+                </div>
+            </div>
+            <div>
+                <p>Welcome to [Your Company Name], your premier choice for personalized small airplane flight services. We are dedicated to providing a unique and unforgettable flying experience, whether you're looking for a scenic tour, a private charter, or an aerial photography session. Our fleet of modern, well-maintained aircraft and our team of experienced pilots ensure your safety and comfort on every journey. At [Your Company Name], we pride ourselves on our exceptional customer service and attention to detail, making your flight experience with us smooth and enjoyable 
+                    from takeoff to landing. Discover the skies like never before with [Your Company Name].</p>
+            </div>
+            
         </div>
     )
 }

eebc1aeb65cdfbb5f7ed2fe2ef4a8f93122c112c
Doidae <andyarredondo1224@gmail.com>
2024-07-11 21:19:37 -0400

    Fixed the title not being aligned with the rest

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const HomePage = () => {
     return (
         <div className="homepage-container">
-            <div className="intro-container">
+            <div>
                 <h1 className="main-title">Welcome to Larry's Airplane Service!</h1>
                 <p className="subtitle">Schedule a flight with us today!</p>
             </div>
